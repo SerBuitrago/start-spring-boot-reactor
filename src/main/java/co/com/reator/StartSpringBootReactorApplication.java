2a9f52101d7687f3ce15a02417eacd22ffdfd566
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-02-02 10:38:54 -0500

    Init

---
+++ src/main/java/co/com/reator/StartSpringBootReactorApplication.java
@@ -0,0 +1,13 @@
+package co.com.reator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringBootReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartSpringBootReactorApplication.class, args);
+	}
+
+}
