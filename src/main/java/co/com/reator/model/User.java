075da9bf12855bb8796668a698e2e9fe77bdcdfb
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-02-02 11:34:04 -0500

    Add operator map

---
+++ src/main/java/co/com/reator/model/User.java
@@ -0,0 +1,13 @@
+package co.com.reator.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	private String name;
+	private String subname;
+}
