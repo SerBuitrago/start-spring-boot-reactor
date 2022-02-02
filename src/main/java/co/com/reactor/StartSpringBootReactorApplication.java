c83f7d411334e7a2fdf66260809859fcad7c14d5
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-02-02 16:43:34 -0500

    Interval infinite and exception

--- src/main/java/co/com/reactor/StartSpringBootReactorApplication.java
+++ src/main/java/co/com/reactor/StartSpringBootReactorApplication.java
@@ -15,6 +15,9 @@ import reactor.core.publisher.Flux;
 public class StartSpringBootReactorApplication implements CommandLineRunner {
 
 	private static final Logger logger = LoggerFactory.getLogger(StartSpringBootReactorApplication.class);
+	
+	private Integer second = 1;
+	private Integer maxTimeout = 5;
 
 	public static void main(String... args) {
 		SpringApplication.run(StartSpringBootReactorApplication.class, args);
@@ -22,17 +25,32 @@ public class StartSpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		latch();
+		latchII();
 	}
 	
 	void latch() throws InterruptedException {
-		CountDownLatch latch = new CountDownLatch(1);
+		CountDownLatch latch = new CountDownLatch(second);
 		
-		Flux.interval(Duration.ofSeconds(1))
+		Flux.interval(Duration.ofSeconds(second))
 		.doOnTerminate(latch::countDown)
 		.map(value -> "Hola "+value)
 		.doOnNext(logger::info)
 		.subscribe();
+		
+		latch.await();
+	}
+	
+	void latchII() throws InterruptedException {
+		CountDownLatch latch = new CountDownLatch(second);
+		
+		Flux.interval(Duration.ofSeconds(second))
+		.doOnTerminate(latch::countDown)
+		.flatMap(value -> {
+			return (value >= maxTimeout) ? Flux.error(new InterruptedException("Solo hasta "+maxTimeout+"!")) : Flux.just(value);
+		})
+		.map(value -> "Hola "+value)
+		.doOnNext(logger::info)
+		.subscribe(System.out::println, error -> logger.error(error.getMessage()));
 		
 		latch.await();
 	}
