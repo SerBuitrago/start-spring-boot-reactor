c11df7340a4e8a3cd048bbd4486d95a270fe0172
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-02-02 13:38:18 -0500

    Add operator flatMap

--- src/main/java/co/com/reator/StartSpringBootReactorApplication.java
+++ src/main/java/co/com/reator/StartSpringBootReactorApplication.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import co.com.reator.model.User;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class StartSpringBootReactorApplication implements CommandLineRunner {
@@ -33,28 +34,15 @@ public class StartSpringBootReactorApplication implements CommandLineRunner {
 		namesList.add("Rafael Gustavo,Barrios");
 		namesList.add("Katherine,Buitrago Mendoza");
 
-		Flux<String> names = Flux.fromIterable(namesList);
-
-		Flux<User> users = names.map(name -> {
+		Flux.fromIterable(namesList).map(name -> {
 			String[] nameAndSubname = name.split(",");
 			return new User(nameAndSubname[0].toUpperCase(), nameAndSubname[1].toUpperCase());
-		}).filter(user -> user.getSubname().equalsIgnoreCase(FILTER_SUBNAME)).doOnNext(user -> {
-			if (user == null)
-				throw new RuntimeException("El usuario no puede estar vacio.");
-			System.out.println(user);
+		}).flatMap(user -> {
+			return (user.getSubname().equalsIgnoreCase(FILTER_SUBNAME)) ? Mono.just(user) : Mono.empty();
 		}).map(user -> {
-			String name = user.getName().toLowerCase();
-			user.setName(name);
+			user.setName(user.getName().toLowerCase());
+			user.setSubname(user.getSubname().toLowerCase());
 			return user;
-		});
-
-		users.subscribe(user -> logger.info(user.toString()), error -> logger.error(error.getMessage()),
-				new Runnable() {
-					@Override
-					public void run() {
-						logger.info("Ha finalizado el observable con exito!");
-
-					}
-				});
+		}).subscribe(user -> logger.info(user.toString()));
 	}
 }
