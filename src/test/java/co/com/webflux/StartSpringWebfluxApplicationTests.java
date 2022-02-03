7759c5a08a6787d0cb003568fe41ab3578fe826e
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-02-03 10:54:11 -0500

    Init webflux

--- src/test/java/co/com/webflux/StartSpringWebfluxApplicationTests.java
+++ src/test/java/co/com/webflux/StartSpringWebfluxApplicationTests.java
@@ -1,10 +1,10 @@
-package co.com.reator;
+package co.com.webflux;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StartSpringBootReactorApplicationTests {
+class StartSpringWebfluxApplicationTests {
 
 	@Test
 	void contextLoads() {
